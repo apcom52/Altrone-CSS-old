94533c99a6df20064caaad616fb22f1ef1fd4d4b
apcom52 <apcom@live.ru>
2015-08-21 19:39:55 +0300

    1.3.8
    + Добавлен модуль Tooltip

--- dist/js/altrone.js
+++ dist/js/altrone.js
@@ -1,18 +1,45 @@
 $(document).ready(function() {
-	$('.modal-open').click(function(event) {
-		event.preventDefault();
-		$('.overlay').fadeIn(200);
-		var modal = '#' + $(this).data('modalId');
-		modal = $(modal);
-		var modalHeight = modal.height()
-		modal.css({'margin-top': -modalHeight / 2}); //установка модального окна по центру
-		modal.fadeIn(300);
-		console.log('start modal');
-	});
 
-	$('.closeModal, .overlay').click(function(event) {
-		event.preventDefault();
-		$('.overlay').fadeOut(200);
-		$('.modal').fadeOut(100);
-	});
+
+	/* Modal Plugin */
+	$('[data-modal-href]').click(function() {
+		var overlay_disable = $(this).data('overlayDisable');		
+		var modal = $('#' + $(this).data('modalHref'));
+		modal.css({'margin-top': -modal.height()/2})
+		$('.overlay').fadeIn(300);
+		modal.fadeIn(350);
+
+		if (overlay_disable === true) {
+			$('.closeModal').click(function() {
+				hideModal();
+			});
+		} else {
+			$('.overlay, .closeModal').click(function() {
+				hideModal();
+			});
+		}		
+	});	
+
+	function hideModal() {
+		$('.modal').fadeOut(200);
+		$('.overlay').fadeOut(250);
+	}
+
+
+	/* Tooltip Plugin */
+
+	$('[data-tooltip]').mousemove(function(event) {
+		var title = $(this).data('tooltipTitle');
+		var title_html = '';
+		if (title) {
+			title_html = '<h1>' + title + '</h1>';
+		} else {
+		}
+		parent = $(this);
+		$('.tooltip').html(title_html + parent.data('tooltip')).css({'top': event.pageY - 30, 'left': event.pageX + 5}).fadeIn(150)
+	})
+
+	$('[data-tooltip]').mouseout(function() {
+		$('.tooltip').fadeOut(100).html('');
+	})
 });
